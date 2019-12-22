a72ff6e604cd4f0cd27b7c2ca3802a2c553617d8
okeicalm <okeicalm@gmail.com>
2019-12-22 15:43:44 +0900

    Fix to valid factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,11 @@
 
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.unique.name}
-    email { Faker::Internet.email  }
-    encrypted_password { Faker::Crypto.sha256 }
+    password = Faker::Internet.password(8)
+
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.email }
+    password { password }
+    password_confirmation { password }
   end
 end
