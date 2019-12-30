5c756698da79d7433041464e0a3786e16de3d58f
okeicalm <okeicalm@gmail.com>
2019-12-30 15:54:52 +0900

    Add route for get /api/projects

--- spec/requests/api/projects_controller_spec.rb
+++ spec/requests/api/projects_controller_spec.rb
@@ -3,6 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Api::ProjectsController, type: :request do
+  describe 'GET /api/projects' do
+    include_context 'with user and auth headers'
+
+    subject { get '/api/projects', headers: auth_headers }
+
+    before(:each) { create_list(:project, 3) }
+
+    it do
+      subject
+      expect(response.status).to eq 200
+      actual_data = JSON.parse(response.body)
+      expect(actual_data.first).to include('id', 'name', 'user_id', 'created_at', 'updated_at')
+      expect(actual_data.count).to eq 3
+    end
+  end
+
   describe 'POST /api/projects' do
     include_context 'with user and auth headers'
 
