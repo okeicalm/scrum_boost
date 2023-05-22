0847031593776a62af40b42979410eae4933200b
okeicalm <okeicalm@gmail.com>
2023-05-23 00:42:52 +0900

    Revert.

--- api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Databases.kt
+++ api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Databases.kt
@@ -1,7 +1,7 @@
 package com.okeicalm.scrumBoost.plugins
 
 import io.ktor.server.application.Application
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabases() {
     @Suppress("unused")
--- api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Security.kt
+++ api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Security.kt
@@ -1,6 +1,6 @@
 package com.okeicalm.scrumBoost.plugins
 
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
 
 @Suppress("EmptyFunctionBlock")
 fun Application.configureSecurity() {}
