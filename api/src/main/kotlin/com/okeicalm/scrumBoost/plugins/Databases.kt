2915f07d13cf93bec97bd1fa9e366cc95cdde27e
okeicalm <okeicalm@gmail.com>
2023-05-24 01:09:44 +0900

    Fix for detekt.

--- api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Databases.kt
+++ api/src/main/kotlin/com/okeicalm/scrumBoost/plugins/Databases.kt
@@ -8,23 +8,23 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
-    @Suppress("unused")
-    val database = Database.connect(
+    Database.connect(
         url = "jdbc:mysql://127.0.0.1:3306/scrum_boost",
         user = "root",
         driver = "com.mysql.cj.jdbc.Driver",
         password = "password"
     )
-    val schema = Schema("scrum_boost").also {
+    Schema("scrum_boost").also {
         transaction {
             SchemaUtils.createSchema(it)
         }
     }
 
-    val tables = arrayOf(
+    arrayOf(
         UserTable
     ).also {
         transaction {
+            @Suppress("SpreadOperator")
             SchemaUtils.createMissingTablesAndColumns(*it)
         }
     }
